00564e9310c9f477140485a1afd7b221f9181425
telleswq <telleswq@gmail.com>
2025-06-02 17:08:49 -0300

    Aprendendo Python

---
+++ dia2/elif.py
@@ -0,0 +1,20 @@
+# %%
+
+idade = 17
+
+if idade >= 70:
+    print("Cuidado com a bebida.")
+    print("Consulte seu geriatra!")
+
+elif idade >= 18:
+    print("Você pode beber cerveja!")
+    print("Beba com moderação!")
+
+elif idade == 17:
+    print("Você ainda não pode beber cerveja!")
+    print("Fique no refri!")
+
+else:
+    print("Você não pode beber!")
+    print("Vá para casa beber leite!")
+
