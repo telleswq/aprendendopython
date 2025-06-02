1a3f23326f2a77006994b724a6fc4331ad3c7cd6
telleswq <telleswq@gmail.com>
2025-06-02 16:51:30 -0300

    Aprendendo python

---
+++ dia2/if.py
@@ -0,0 +1,13 @@
+# %%
+
+idade = 15
+
+if idade >= 18:
+    print("Você pode beber cerveja!")
+    print("Beba com moderação!")
+
+if idade <= 17:
+    print("Você não pode beber!")
+    print("Vá para casa beber leite!")
+
+
