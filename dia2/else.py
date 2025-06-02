f3d03e9bb28e82cbf2c09653e392291de2f98cdb
telleswq <telleswq@gmail.com>
2025-06-02 17:00:11 -0300

    Aprendendo python

---
+++ dia2/else.py
@@ -0,0 +1,13 @@
+# %%
+
+idade = 15
+
+if idade >= 18:
+    print("Você pode beber cerveja!")
+    print("Beba com moderação!")
+
+else:
+    print("Você não pode beber!")
+    print("Vá para casa beber leite!")
+
+
