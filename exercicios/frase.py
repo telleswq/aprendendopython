0ccba05d20f2b4fe980c69b92debb9232f371235
telleswq <telleswq@gmail.com>
2025-06-05 15:42:24 -0300

    Aprendendo python

--- exercicios/frase.py
+++ exercicios/frase.py
@@ -1,5 +1,14 @@
+dados = {}
 
 while True:
     frase = input("Entre com a frase: ")
     if frase == "":
         break
+
+    if frase not in dados:
+        dados[frase] = 1
+    else:
+        dados[frase] += 1
+
+for i, j in dados.items():
+    print(i, "->", j)
